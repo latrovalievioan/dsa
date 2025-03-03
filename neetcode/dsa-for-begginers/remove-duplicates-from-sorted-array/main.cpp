5a5f6a1cba8aa724bfb1d1b27966d9ddc1813e72
Ioan Latrovaliev <latrovalievioan@gmail.com>
2025-03-03 19:21:25 +0200

    solve remove-duplicates

---
+++ neetcode/dsa-for-begginers/remove-duplicates-from-sorted-array/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int lastUnique = 0; 
+
+        for (int i = 1; i < nums.size(); i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[lastUnique + 1] = nums[i];
+
+                lastUnique++;
+            }
+        };
+
+        return lastUnique + 1;
+    }
+};
+
+int main() {
+    vector<int> nums = {0, 1, 1, 2, 3};
+    Solution solution;
+    int result = solution.removeDuplicates(nums);
+
+    cout << result << std::endl;
+
+    return 0;
+}
